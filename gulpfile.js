173739754dcc34be0521588b4a30a50cb85b0042
Salih Genc <salihgenc34@gmail.com>
2021-07-25 22:47:38 +0300

    gulp bug

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require("gulp");
+var gulpless = require("gulp-less");
+
+gulp.task("lesstocss", function () {
+  var srcfile = "./less/main.less";
+  var temp = "./dist/main.css";
+  return gulp.src(srcfile).pipe(gulpless()).pipe(gulp.dest(temp));
+});
+
+
